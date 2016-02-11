69590563a57e0369004064d195f43dad796a1991
Kyle Schouten <kschouten@compete.nl>
2016-02-11 21:32:06 +0100

    ExpressionLines summary added

--- CodeGen/CodeGen/ExpressionLines.cs
+++ CodeGen/CodeGen/ExpressionLines.cs
@@ -4,27 +4,51 @@ using System.Linq;
 
 namespace CodeGen
 {
+
+    /// <summary>
+    /// IExpressionLine is an Interface used to define a ExpressionLine class.
+    /// </summary>
     public interface IExpressionLine
     {
-
+        /// <summary>
+        /// CreateExpression generates a CodeExpression based on the given member data.
+        /// </summary>
+        /// <returns>Generated CodeExpression.</returns>
         CodeExpression CreateExpression();
     }
 }
 
 namespace CodeGen.Expressions
 {
+    /// <summary>
+    /// ArgumentReferenceExpressionLine is a class for simply creating a Argument Expression.
+    /// </summary>
     public class ArgumentReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// CreateExpression generates a CodeExpression based on the given member data.
+        /// </summary>
+        /// <returns>Generated CodeExpression.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeArgumentReferenceExpression(ParameterName);
         }
 
+        /// <summary>
+        /// The name of the paramter to reference.
+        /// </summary>
         public string ParameterName { get; set; }
     }
 
+    /// <summary>
+    /// ArrayCreateExpressionLine is a class for simply creating a Array create Expression.
+    /// </summary>
     public class ArrayCreateExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// CreateExpression generates a CodeExpression based on the given member data.
+        /// </summary>
+        /// <returns>Generated CodeExpression.</returns>
         public CodeExpression CreateExpression()
         {
             var createType = new CodeTypeReference(Type);
@@ -33,91 +57,183 @@ namespace CodeGen.Expressions
                 : new CodeArrayCreateExpression(createType, Size);
         }
 
+        /// <summary>
+        /// The number of indexes of the array.
+        /// </summary>
         public int Size { get; set; }
 
+        /// <summary>
+        /// An Array of IExpressionLines used to intialize the array.
+        /// </summary>
         public IExpressionLine[] Initializers { get; set; }
 
+        /// <summary>
+        /// Type that indicates of the array to create.
+        /// </summary>
         public Type Type { get; set; }
     }
 
+    /// <summary>
+    /// CodeArrayIndexerExpressionLine is a class for simply creating a Array Indexer Expression.
+    /// </summary>
     public class CodeArrayIndexerExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// CreateExpression generates a CodeExpression based on the given member data.
+        /// </summary>
+        /// <returns>Generated CodeExpression.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeArrayIndexerExpression(TargetObject.CreateExpression(),
                 Indices.Select(i => i.CreateExpression()).ToArray());
         }
 
+        /// <summary>
+        /// Indexes to reference.
+        /// </summary>
         public IExpressionLine[] Indices { get; set; }
 
+        /// <summary>
+        /// An IExpressionLine that indicates the indexer targets.
+        /// </summary>
         public IExpressionLine TargetObject { get; set; }
     }
 
+    /// <summary>
+    /// BaseReferenceExpressionLine is a class for simply creating a Base Reference Expression.
+    /// </summary>
     public class BaseReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// CreateExpression generates a CodeExpression based on the given member data.
+        /// </summary>
+        /// <returns>Generated CodeExpression.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeBaseReferenceExpression();
         }
     }
 
+    /// <summary>
+    /// BinaryOperatorExpressionLine is a class for simply creating a Binary operator Expression.
+    /// </summary>
     public class BinaryOperatorExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// CreateExpression generates a CodeExpression based on the given member data.
+        /// </summary>
+        /// <returns>Generated CodeExpression.</returns>
         public CodeExpression CreateExpression()
         {
             CodeBinaryOperatorType result;
             if (Enum.TryParse(OperatorType, out result))
-                return new CodeBinaryOperatorExpression(Left.CreateExpression(), result, Rights.CreateExpression());
+                return new CodeBinaryOperatorExpression(Left.CreateExpression(), result, Right.CreateExpression());
             throw new Exception("OperatorType Not Found.");
         }
 
+        /// <summary>
+        /// Type of Operator.
+        /// </summary>
         public string OperatorType { get; set; }
 
-        public IExpressionLine Rights { get; set; }
+        /// <summary>
+        ///  IExpressionLine for the Right side of the operator.
+        /// </summary>
+        public IExpressionLine Right { get; set; }
 
+        /// <summary>
+        ///  IExpressionLine for the Left side of the operator.
+        /// </summary>
         public IExpressionLine Left { get; set; }
     }
 
+    /// <summary>
+    /// CastExpressionLine is a class for simply creating a Cast Expression.
+    /// </summary>
     public class CastExpressionLine : IExpressionLine
     {
+        /// <summary>
+		/// Create a statementLine using the given member data.
+		/// </summary>
+		/// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeCastExpression(TargetType, Expression.CreateExpression());
         }
 
+        /// <summary>
+        ///  IExpressionLine to cast.
+        /// </summary>
         public IExpressionLine Expression { get; set; }
 
+        /// <summary>
+        /// The Destination type of the cast.
+        /// </summary>
         public Type TargetType { get; set; }
     }
 
+    /// <summary>
+    /// DefaultValueExpressionLine is a class for simply creating a Default Value Expression.
+    /// </summary>
     public class DefaultValueExpressionLine : IExpressionLine
     {
+        /// <summary>
+		/// Create a statementLine using the given member data.
+		/// </summary>
+		/// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             var type = new CodeTypeReference(Type);
             return new CodeDefaultValueExpression(type);
         }
 
+        /// <summary>
+        /// Type that specifies the reference to a value type
+        /// </summary>
         public Type Type { get; set; }
     }
 
+    /// <summary>
+    /// DelegateCreateExpressionLine is a class for simply creating a Delegate Create Expression.
+    /// </summary>
     public class DelegateCreateExpressionLine : IExpressionLine
     {
+
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             var delegateType = new CodeTypeReference(Type);
             return new CodeDelegateCreateExpression(delegateType, TargetObject.CreateExpression(), MethodeName);
         }
 
+        /// <summary>
+        /// Type that indicates the data type of the delegate.
+        /// </summary>
         public Type Type { get; set; }
 
+        /// <summary>
+        /// An IExpressionLine that indicates the object containing the event-handler method.
+        /// </summary>
         public IExpressionLine TargetObject { get; set; }
 
+        /// <summary>
+        /// The name of the event-handler method.
+        /// </summary>
         public string MethodeName { get; set; }
     }
 
+    /// <summary>
+    /// DelegateInvokeExpressionLine is a class for simply creating a Delegate Invoke Expression.
+    /// </summary>
     public class DelegateInvokeExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return Parameters != null
@@ -126,13 +242,26 @@ namespace CodeGen.Expressions
                 : new CodeDelegateInvokeExpression(TargetObject.CreateExpression());
         }
 
+        /// <summary>
+        /// Array that indicate the parameters. 
+        /// </summary>
         public IExpressionLine[] Parameters { get; set; }
 
+        /// <summary>
+        /// Expression that indicates the target object.
+        /// </summary>
         public IExpressionLine TargetObject { get; set; }
     }
 
+    /// <summary>
+    /// DirectionExpressionLine is a class for simply creating a Direction  Expression.
+    /// </summary>
     public class DirectionExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             FieldDirection direction;
@@ -141,13 +270,23 @@ namespace CodeGen.Expressions
             throw new Exception("FieldDirection Not Found.");
         }
 
+        /// <summary>
+        /// Direction that inid
+        /// </summary>
         public string Direction { get; set; }
-
+        
+        /// <summary>
+        /// IExpression that indicates the code expression to represent.
+        /// </summary>
         public IExpressionLine Expression { get; set; }
     }
 
     public class EventReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeEventReferenceExpression(TargetObject.CreateExpression(), EventName);
@@ -160,6 +299,10 @@ namespace CodeGen.Expressions
 
     public class FieldReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeFieldReferenceExpression(TargetObject.CreateExpression(), FieldName);
@@ -172,6 +315,10 @@ namespace CodeGen.Expressions
 
     public class IndexerExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeIndexerExpression(TargetObject.CreateExpression(),
@@ -185,6 +332,10 @@ namespace CodeGen.Expressions
 
     public class MethodInvokeExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
 
@@ -201,6 +352,10 @@ namespace CodeGen.Expressions
 
     public class MethodReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             if (Types == null) return new CodeMethodReferenceExpression(TargetObject.CreateExpression(), MethodName);
@@ -217,6 +372,10 @@ namespace CodeGen.Expressions
 
     public class ObjectCreateExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeObjectCreateExpression(CreateType, Parameters.Select(p => p.CreateExpression()).ToArray());
@@ -229,6 +388,10 @@ namespace CodeGen.Expressions
 
     public class ParameterDeclarationExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeParameterDeclarationExpression(Type, Name);
@@ -241,6 +404,11 @@ namespace CodeGen.Expressions
 
     public class PrimitiveExpressionLine : IExpressionLine
     {
+
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodePrimitiveExpression(Value);
@@ -251,6 +419,10 @@ namespace CodeGen.Expressions
 
     public class PropertyReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodePropertyReferenceExpression(TargetObject.CreateExpression(), PropertyName);
@@ -263,6 +435,10 @@ namespace CodeGen.Expressions
 
     public class PropertySetValueReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodePropertySetValueReferenceExpression();
@@ -271,6 +447,10 @@ namespace CodeGen.Expressions
 
     public class SnippetExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeSnippetExpression(Value);
@@ -281,6 +461,10 @@ namespace CodeGen.Expressions
 
     public class ThisReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeThisReferenceExpression();
@@ -289,6 +473,10 @@ namespace CodeGen.Expressions
 
     public class TypeOfExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeTypeOfExpression(Type);
@@ -299,6 +487,10 @@ namespace CodeGen.Expressions
 
     public class TypeReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeTypeReferenceExpression(Type);
@@ -309,6 +501,10 @@ namespace CodeGen.Expressions
 
     public class VariableReferenceExpressionLine : IExpressionLine
     {
+        /// <summary>
+        /// Create a statementLine using the given member data.
+        /// </summary>
+        /// <returns>Returns the generated CodeStatement.</returns>
         public CodeExpression CreateExpression()
         {
             return new CodeVariableReferenceExpression(VariableName);
